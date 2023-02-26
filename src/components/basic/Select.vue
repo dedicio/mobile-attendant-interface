c571bda6777ff58fc1a7126ce043b26c1238468b
Dedicio Coelho <dedicio@gmail.com>
2023-02-26 12:54:53 -0300

    feat: create field select

---
+++ src/components/basic/Select.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+type StringOrNumber = string | number
+
+interface Props {
+  modelValue?: StringOrNumber
+}
+
+interface Emits {
+  (e: 'update:modelValue', value?: StringOrNumber): void
+}
+
+interface Option {
+  label: string,
+  id: string,
+}
+
+const props = defineProps<Props>()
+const emit = defineEmits<Emits>()
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
+
+const options: Option[] = [
+  {
+    label: 'Mesa 1',
+    id: '1',
+  },
+  {
+    label: 'Mesa 3',
+    id: '3',
+  },
+  {
+    label: 'Mesa 6',
+    id: '6',
+  }
+]
+</script>
+          
+<template>
+  <select
+    v-model="value"
+    class="bg-white border-blue rounded w-full border p-1">
+    <option
+      disabled
+      :value="undefined">Selecione uma mesa</option>
+    <option
+      v-for="option in options"
+      :key="option.id"
+      :value="option.id">
+      {{ option.label }}
+    </option>
+  </select>
+</template>
